c85af736853d6838908c642436c6d93fefafe13e
ramon149 <ramon149@yandex.ru>
2018-03-08 03:52:18 +0300

    Update task_9.php

--- lesson_3/task_9.php
+++ lesson_3/task_9.php
@@ -9,7 +9,7 @@ function translit($str){
 		'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'zh','з' => 'z', 'и' => 'i', 'й' => 'i', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch','ь' => '', 'ы' => 'y', 'ъ' => '',  'ш' => 'sh','щ' => 'shch', 'э' => 'e', 'ю' => 'yu','я' => 'ya','А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'E', 'Ж' => 'Zh','З' => 'Z', 'И' => 'I', 'Й' => 'I', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C', 'Ч' => 'Ch','Ш' => 'Sh','Щ' => 'Shch', 'Ь' => '',  'Ы' => 'Y', 'Ъ' => '',  'Э' => 'E', 'Ю' => 'Yu','Я' => 'Ya',
 	);
 
-    $result_str = preg_replace('/\s/', '_', strtr($str, $arr));
+	$result_str = preg_replace('/\s/', '_', strtr($str, $arr));
 	return  $result_str;
 }
 echo(translit('Привет! Что нового? - Изучаю PHP!'));
@@ -23,7 +23,7 @@ function translitCycle($str){
 
 	for ($i=0;$i<mb_strlen($str);$i++){
 		$let = mb_substr($str, $i, 1);
-        $result_str .= (preg_match('/\s/','_', $let)) ? $let : $arr[$let];
+        	$result_str .= (preg_match('/\s/','_', $let)) ? $let : $arr[$let];
 	}
 
 	return $result_str;
